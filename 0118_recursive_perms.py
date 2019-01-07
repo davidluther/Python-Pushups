24d39a88796ec5deea249a8881cb81bcf933b98c
David Luther <davidrluther@gmail.com>
2019-01-06 23:21:24 -0600

    added functionality to permutations

--- 0118_recursive_perms.py
+++ 0118_recursive_perms.py
@@ -2,25 +2,31 @@
 # initially account for duplicates
 
 
-def perms_with_dupes(str_in):
+def perms_with_dupes(collection):
     """Returns a list of permutations for an input string"""
 
     # base cases
-    if len(str_in) == 1:
-        return [str_in]
-    if len(str_in) == 2:
-        return [str_in, str_in[::-1]]
+    if len(collection) == 1:
+        return [collection]
+    if len(collection) == 2:
+        return [collection, collection[::-1]]
     # recursion
     else:
-        plist = []
-        for ix, char in enumerate(str_in):
-            if ix == len(str_in)-1:
-                not_char = str_in[:-1]
+        perms = []
+        for ix, item in enumerate(collection):
+            if ix == len(collection)-1:
+                excl_items = collection[:-1]
             else:
-                not_char = str_in[:ix] + str_in[ix+1:]
-            for perm in perms_with_dupes(not_char):
-                plist.append(char + perm)
-        return plist
+                excl_items = collection[:ix] + collection[ix+1:]
+            print("Item", item)
+            print("Excl_items", excl_items)
+            for perm in perms_with_dupes(excl_items):
+                if isinstance(collection, list) and not isinstance(item, list):
+                    item = [item]
+                # print("Item:", item)
+                # print("Perm:", perm)
+                perms.append(item + perm)
+        return perms
 
 
 def perms_no_dupes(str_in):
@@ -36,7 +42,7 @@ def perms_no_dupes(str_in):
             return [str_in, str_in[::-1]]
     # recursion
     else:
-        plist = []
+        perms = []
         chars = set()
         for ix, char in enumerate(str_in):
             if char in chars:
@@ -46,9 +52,9 @@ def perms_no_dupes(str_in):
             else:
                 not_char = str_in[:ix] + str_in[ix+1:]
             for perm in perms_no_dupes(not_char):
-                plist.append(char + perm)
+                perms.append(char + perm)
             chars.add(char)
-        return plist
+        return perms
 
 
 def permutations(str_in, remove_dupes=True):
@@ -65,7 +71,7 @@ def permutations(str_in, remove_dupes=True):
             return [str_in, str_in[::-1]]
     # recursion
     else:
-        plist = []
+        perms = []
         if remove_dupes:
             chars = set()
         for ix, char in enumerate(str_in):
@@ -79,18 +85,51 @@ def permutations(str_in, remove_dupes=True):
             else:
                 not_char = str_in[:ix] + str_in[ix+1:]
             for perm in permutations(not_char, remove_dupes=remove_dupes):
-                plist.append(char + perm)
-        return plist
+                perms.append(char + perm)
+        return perms
+
+
+def permutations_plus(collection, k=None):
+    """
+    Returns list of all k-length permutations from collection.
+    """
+    if not isinstance(collection, list):
+        collection = list(collection)
+    if k is None:
+        k = len(collection)
+
+    if k == 1:
+        return [[item] for item in collection]
+    else:
+        perms = []
+        for ix, item in enumerate(collection):
+            if ix == len(collection)-1:
+                excl_items = collection[:-1]
+            else:
+                excl_items = collection[:ix] + collection[ix+1:]
+            for perm in permutations_plus(excl_items, k-1):
+                perms.append([item] + perm)
+        return perms
 
 
 if __name__ == "__main__":
     while True:
-        str_in = input("\nA string: ")
-        if input("Remove duplicates? (y/n): ").lower() == 'y':
-            rm_dupes = True
-        else:
-            rm_dupes = False
+        choice = input("\nString (s) or list (l): ")
+        user_in = input("Enter pattern: ")
+        if choice.lower() == 'l':
+            user_in = user_in.split()
 
-        perms = permutations(str_in, remove_dupes=rm_dupes)
-        print(perms)
-        print("Number of permutations:", len(perms))
+        user_k = input("Value of k (int): ")
+        if user_k == '':
+            user_k = None
+        else:
+            user_k = int(user_k)
+
+        # if input("Remove duplicates? (y/n): ").lower() == 'y':
+        #     rm_dupes = True
+        # else:
+        #     rm_dupes = False
+        # perm_results = permutations(user_in, remove_dupes=rm_dupes)
+        perm_results = permutations_plus(user_in, user_k)
+        print(sorted(perm_results))
+        print("Number of permutations:", len(perm_results))
